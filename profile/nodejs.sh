f515656b185bad7ec5aeda0804739c5819f5ce00
Chris Fritz <chrisvfritz@gmail.com>
2018-07-09 10:42:49 -0400

    add node_modules from subfolders to path

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -1,3 +1,3 @@
-export PATH="$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
+export PATH="$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin:$HOME/backend/node_modules/.bin:$HOME/frontend/node_modules/.bin"
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
